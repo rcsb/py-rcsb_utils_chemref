8d6f57752521f9d67d8c60d5491e55d42d29dd3d
jwest <john.westbrook@rcsb.org>
2019-08-29 21:04:36 -0400

    Correct dependencies

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     ),
     entry_points={"console_scripts": []},
     #
-    install_requires=["mmcif >= 0.46", "rcsb.utils.io >= 0.46", "rcsb.utils.config >= 0.23"],
+    install_requires=["mmcif >= 0.46", "rcsb.utils.io >= 0.40", "rcsb.utils.config >= 0.23"],
     packages=find_packages(exclude=["rcsb.mock-data", "rcsb.utils.tests-chemref", "rcsb.utils.tests-*", "tests.*"]),
     package_data={
         # If any package contains *.md or *.rst ...  files, include them:
